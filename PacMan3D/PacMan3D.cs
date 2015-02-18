675fff5efc6715e923a501e3f05788ca7dd70dbb
Marin Jelev <marin.jelev92@gmail.com>
2015-02-18 15:17:37 +0200

    Added initial version of start menu.

--- PacMan3D/PacMan3D.cs
+++ PacMan3D/PacMan3D.cs
@@ -26,6 +26,8 @@ class PacMan3D
         //Method for creating playing field
         SetPlayfieldSize(out playfieldHeight, out playfieldWidth);
 
+        StartupScreen();
+
         // define hero pacMan as a variable of type element
         Element pacMan = new Element();
         // initial pacman position in center of playfield
@@ -115,7 +117,7 @@ class PacMan3D
         }
     }
 
-    private static void SetPlayfieldSize(out int playfieldHeight, out int playfieldWidth)
+    static void SetPlayfieldSize(out int playfieldHeight, out int playfieldWidth)
     {
         // set console size (screen resolution)
         Console.BufferHeight = Console.WindowHeight = 21;
@@ -143,5 +145,156 @@ class PacMan3D
             Console.WriteLine(thisArray[i]);
         }
     }
-}
 
+    /// <summary>
+    /// MY CODE
+    /// </summary>
+    /// 
+    static void StartupScreen()
+    {
+        //TO DO: string pacManLogo = "PAC MAN 3D";
+
+        //Two-dimensional border array (smileyface)
+        Console.CursorVisible = false;
+
+        char[,] smileyFace = new char[Console.BufferHeight, Console.BufferWidth];
+
+        for (int col = 0; col < smileyFace.GetLength(1); col++)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            smileyFace[Console.BufferHeight - 1, col] = (char)9787;
+            smileyFace[1, col] = (char)9787;
+        }
+
+        for (int row = 0; row < smileyFace.GetLength(0); row++)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            smileyFace[row, 0] = (char)9787;
+            smileyFace[row, Console.BufferWidth - 1] = (char)9787;
+        }
+
+        //Printing of border array
+        PrintSmileyArray(smileyFace);
+
+/////
+        int cursorPositionX = Console.BufferWidth / 7;
+        int cursorPositionY = Console.BufferHeight - 5;
+        Console.SetCursorPosition(cursorPositionX, cursorPositionY);
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(">>New Game   Read Instructions");
+
+        int pressedKeyValue = 0;
+        bool check = pressedKeyValue != 0 || pressedKeyValue != 1;
+
+
+
+        if (pressedKeyValue == 0 && Console.ReadKey(true).Key == ConsoleKey.Enter) //The ">>" is initially set at New Game, hence if {ENTER} 
+        {                                                                       //is pressed Game will start
+            Console.Beep();
+            return;
+        }
+        else
+        {
+            while (check)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.LeftArrow)
+                {
+                    pressedKeyValue = 0;
+                    Console.Beep();
+
+                }
+                else if (key == ConsoleKey.RightArrow)
+                {
+                    pressedKeyValue = 1;
+                    Console.Beep();
+                }
+
+                switch (pressedKeyValue)
+                {
+                    case 0:
+                        Console.Clear();
+
+                        PrintSmileyArray(smileyFace);
+
+                        Console.SetCursorPosition(cursorPositionX, cursorPositionY);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(">>New Game    Read Instructions");
+                        break;
+
+                    case 1:
+                        Console.Clear();
+
+                        PrintSmileyArray(smileyFace);
+
+                        Console.SetCursorPosition(cursorPositionX, cursorPositionY);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("New Game    >>Read Instructions");
+                        break;
+
+                }
+
+                if (pressedKeyValue == 0 && key == ConsoleKey.Enter) //">>" will be at "New Game" and after 
+                {                                                                          //pressing {ENTER} game will start.
+                    Console.Beep();
+                    break;
+                }
+
+                if (pressedKeyValue == 1 && key == ConsoleKey.Enter) //">>" will be at "Read Instructions" and after
+                {
+                    Console.Beep();
+                    Console.Clear();                                                       
+                    PrintSmileyArray(smileyFace);
+                    Instructions();
+                    if (key == ConsoleKey.Escape)
+                    {
+                        StartupScreen();
+                    }
+                    else
+                    {
+                        Instructions();
+                    }
+                }
+            }
+        }
+        
+    }
+
+    static void PrintSmileyArray(char[,] smileyFace)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        for (int row = 0; row < smileyFace.GetLength(0); row++)
+        {
+            for (int col = 0; col < smileyFace.GetLength(1); col++)
+            {
+                Console.Write(smileyFace[row, col]);
+            }
+        }
+    }
+
+    static void Instructions()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.SetCursorPosition(Console.BufferWidth / 3, 2);
+        Console.WriteLine("INSTRUCTIONS:");
+        Console.SetCursorPosition(1, 4);
+        Console.WriteLine("Pacman, our hero, moves around the");
+        Console.SetCursorPosition(1, 5);
+        Console.WriteLine("labyrinth, collecting all of the gold");
+        Console.SetCursorPosition(1, 6);
+        Console.WriteLine("pieces and earning points. Once the");
+        Console.SetCursorPosition(1, 7);
+        Console.WriteLine("player has collected a certain amount"); 
+        Console.SetCursorPosition(1, 8);
+        Console.WriteLine("of points, a secret portal opens to");
+        Console.SetCursorPosition(1, 9);
+        Console.WriteLine("the next level. If Pacman is caught by");
+        Console.SetCursorPosition(1, 10);
+        Console.WriteLine("by the enemy, the player loses a life.");
+
+        Console.SetCursorPosition(Console.BufferWidth / 7, Console.BufferHeight - 5);
+        Console.WriteLine("Press Esc to go back...");
+    }
+}
